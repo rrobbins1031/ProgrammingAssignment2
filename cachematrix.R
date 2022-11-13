fe9dc45a5b975160bcad7c223ce2916c80bc4e82
rrobbins1031 <90996075+rrobbins1031@users.noreply.github.com>
2022-11-13 15:22:58 -0700

    cachmatrix.R
    This is my submission for the Coursera R Programming Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program will create a matrix and cache the inverse to allow for later
+## without the need to re-compute the matrix each time.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## First function creates our matrix, gets the inverse, and caches the inverse.
+library(MASS)
+makeCacheMatrix <- function (x = numeric()){
+        inv <- NULL
+        set <- function (y){
+                x <<- y
+                inv <<- NULL
+        }
+        get<- function()x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function(){
+                inver <- ginv(x)
+                inver%*%x   ##obtains the inverse of the matrix
+        }
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function (x, ...){
+        inv <- x$getinv()
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        m=inv ## Returns a matrix being the inverse of 'x'
 }
